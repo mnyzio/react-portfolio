7fc5a1350ffd15852fe46dd7e6a8778dea04785a
mnyzio <nyziu84@gmail.com>
2023-05-12 23:36:36 -0400

    added wildcard route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import Resume from "./pages/Resume";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
       <Route path='/portfolio' element={<Portfolio />} />
       <Route path='/contact' element={<Contact />} />
       <Route path='/resume' element={<Resume />} />
+      <Route path='*' element={<NotFound />} />
     </Routes>
   </>
   )
---
+++ src/pages/NotFound.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+
+function NotFound(){
+    return <h1>NotFound</h1>      
+}
+
+
+export default NotFound;
