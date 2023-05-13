1035d4df15583f131c8bd898ea21b1dd30b93778
mnyzio <nyziu84@gmail.com>
2023-05-13 14:04:27 -0400

    added tailwindcss library

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+// import { useState, useEffect } from 'react';
 
 function Portfolio(){
     return <h1>Portfolio</h1>      
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+// import { useState, useEffect } from 'react';
 
 function Resume(){
     return <h1>Resume</h1>      
