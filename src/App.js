17ec3e260bb5413a9a0e181dc6fbb5d68bb64b89
mnyzio <nyziu84@gmail.com>
2023-05-12 23:23:17 -0400

    added React Router and created basic pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { Link, Route, Routes } from "react-router-dom";
 import './App.css';
+import Home from './pages/Home';
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <nav>
+      <ul>
+        <li><Link to={'/'}>Home</Link></li>
+        <li><Link to={'/about'}>About</Link></li>
+        <li><Link to={'/portfolio'}>Portfolio</Link></li>
+        <li><Link to={'/contact'}>Contact</Link></li>
+        <li><Link to={'/resume'}>Resume</Link></li>
+      </ul>  
+    </nav>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/portfolio' element={<Portfolio />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/resume' element={<Resume />} />
+    </Routes>
+  </>
+  )
 }
 
 export default App;
---
+++ src/pages/Resume.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+
+function Resume(){
+    return <h1>Resume</h1>      
+}
+
+
+export default Resume;
