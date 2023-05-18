a1276ff442bbafc5afc405293cf188d949738a1d
mnyzio <nyziu84@gmail.com>
2023-05-17 22:23:40 -0400

    testing hash router

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/react-portfolio/'>
+    <HashRouter>
+      {/* <BrowserRouter basename='/react-portfolio/'> */}
       <App />
-    </BrowserRouter>
+      {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>
 );
