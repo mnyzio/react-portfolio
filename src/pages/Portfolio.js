16087a5f9a4d1c0f4bce5f70f50bd6a478537cff
mnyzio <nyziu84@gmail.com>
2023-05-18 22:02:12 -0400

    moved each project to Project component

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,22 +1,17 @@
 import { motion as m } from "framer-motion";
 import Footer from "../components/Footer";
-import { Link } from "react-router-dom";
+import Project from "../components/Project";
 
 const container = {
   hidden: { opacity: 0 },
   show: {
     opacity: 1,
     transition: {
-      delayChildren: 0.8,
+      staggerChildren: .2,
       duration: 0.8,      
       ease: "easeInOut",
     },
   },
-};
-
-const item = {
-  hidden: { opacity: 0 },
-  show: { opacity: 1 },
 };
 
 export default function Portfolio(props) {
@@ -41,36 +36,8 @@ export default function Portfolio(props) {
         >
           {/* Loop through all the projects creating card for each */}
           {/* project card */}
-          {props.projects.map((project) => (
-            <m.div
-              variants={item}
-              className="mdax-w-sm rounded overflow-hidden shadow-lg border transition-all duration-300 hover:scale-105 hover:shadow-xl"
-              key={project.id}
-            >
-              <img
-                className="w-full"
-                src={project.imageSrc}
-                alt={project.imageAlt}
-              />
-              <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">
-                  {project.projectName}
-                </div>
-                <p className="text-base">{project.projectDesc}</p>
-              </div>
-              <div className="px-6 pt-4 pb-2">
-                <Link to={project.linkDeployed} target="_blank">
-                  <div className="inline-block bg-orange rounded-lg px-3 py-1 text-sm font-semibold text-white mr-2 mb-2 hover:opacity-90 transition-all duration-300 shadow-lg">
-                    Deployed website
-                  </div>
-                </Link>
-                <Link to={project.linkGitHub} target="_blank">
-                  <div className="inline-block bg-orange rounded-lg px-3 py-1 text-sm font-semibold text-white mr-2 mb-2 hover:opacity-90 transition-all duration-300 shadow-lg">
-                    GitHub Repo
-                  </div>
-                </Link>
-              </div>
-            </m.div>
+          {props.projects.map((project) => (            
+            <Project project={project} />
           ))}
         </m.div>
       </m.div>
