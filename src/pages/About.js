d5d8c76e3e9f6fee240c4bfed5eea0f57ae66f63
mnyzio <nyziu84@gmail.com>
2023-05-19 20:22:55 -0400

    about me component

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,25 +1,54 @@
 import { motion as m } from "framer-motion";
 import Footer from "../components/Footer";
+import profilePic from "../img/Profile-picture-bw.jpg";
 
 export default function About() {
   return (
     <>
-    <m.div
-      initial={{ y: "10", opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 0.75, ease: "easeOut" }}
-      exit={{ opacity: 0 }}
-      className="py-4 mt-8"
-    >
-      <h3 className="text-5xl font-bold">About</h3>
-      <p className="p-3">
-        Hi there! I'm Mirek, and I am a Full Stack Developer with background in
-        logistics and operations. Passionale developer / coder with solid
-        foundation in software development. Calm under pressure, quick learner
-        with the goal of customer satisfaction.
-      </p>
-    </m.div>
-    <Footer />
+      <m.div
+        initial={{ y: "10", opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.75, ease: "easeOut" }}
+        exit={{ opacity: 0 }}
+        className="py-4 mt-8"
+      >
+        <h3 className="text-5xl font-bold mb-8">About</h3>
+        {/* Hero section */}
+        <section
+          id="hero-wrapper"
+          className="flex flex-col items-center md:flex-row md:justify-evenly md:mb-12"
+        >
+          {/* Left / text side of hero section */}
+          <div
+            id="hero-name"
+            className="w-full md:w-1/2 flex flex-col items-center"
+          >
+            <h1 className="z-10 text-8xl font-extrabold">Mirek</h1>
+            <h1 className="z-10 text-8xl font-extrabold">Nyzio</h1>
+            <div
+              id="hero-sub-text-container"
+              className="relative z-0 left-2 text-white bg-orange px-8 py-2 text-xl rounded-md shadow-lg font-semibold -rotate-3"
+            >
+              <p id="hero-sub-text">Full-Stack Web Developer</p>
+            </div>
+          </div>
+          {/* Right / avatar side of hero section */}
+          <div id="hero-avatar" className="w-full flex justify-center mt-4 md:mt-0 md:w-1/2">
+            <img
+              src={profilePic}
+              alt="Avatar"
+              className="w-56 md:w-80 rounded-full shadow-lg"
+            />
+          </div>
+        </section>
+        <p className="p-12">
+          Hi there! I'm Mirek, and I am a Full Stack Developer with background
+          in logistics and operations. Passionale developer / coder with solid
+          foundation in software development. Calm under pressure, quick learner
+          with the goal of customer satisfaction.
+        </p>
+      </m.div>
+      <Footer />
     </>
   );
 }
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -11,7 +11,51 @@ export default function Resume() {
         exit={{ opacity: 0 }}
         className="py-4 mt-8"
       >
+        {/* Title */}
         <h3 className="text-5xl font-bold">Resume</h3>
+        {/* Educaiton */}
+        <h5>Education</h5>
+
+        <h5>Skills / Languages</h5>
+        <ul>
+          <li>HTML</li>
+          <li>JavaScript</li>
+          <li>CSS</li>
+          <li>
+            <ul>
+              <li>Bootstrap</li>
+              <li>Tailwind</li>
+              <li>Bulma</li>
+            </ul>
+          </li>
+          <li></li>
+          <li></li>
+        </ul>
+        {/* Download section */}
+        <div className="border border-orange inline-block rounded-lg p-2">
+          <a
+            href="./assets/files/portfolio.pdf"
+            target="_blank"
+            download
+            className="flex"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+              />
+            </svg>
+            <span className="ml-2">Download resume</span>
+          </a>
+        </div>
       </m.div>
       <Footer />
     </>
